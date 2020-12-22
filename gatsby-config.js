d9407347be10d879009ee9ecba6c5653e85a30fc
mailynhuynh <mailynhuynh@gmail.com>
2020-12-22 00:01:45 -0600

    part five

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,14 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`
+      }
+    },
+    `gatsby-plugin-emotion`,
+    {
       resolve: `gatsby-plugin-typography`,
       options: {
         pathToConfigModule: `src/utils/typography`,
